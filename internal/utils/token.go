9af71fc5220006c5a2067b640255f9d3412a7a0c
Nick <seedrider@bk.ru>
2025-06-15 01:12:29 +0500

    тестирую метод

--- internal/utils/token.go
+++ internal/utils/token.go
@@ -2,17 +2,21 @@
 package utils
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var secretKey = []byte(os.Getenv("JWT_SECRET_KEY_USER")) // Должен быть в конфиге!
+
+type ClaimToRecover struct {
+	Email     string    `json:"email"`
+	Role      string    `json:"role"`
+	expiresAt time.Time `json:expires_at`
+	jwt.RegisteredClaims
+}
 
 type ResetToken struct {
 	Email     string    `json:"email"`
@@ -21,66 +25,92 @@ type ResetToken struct {
 }
 
 func GenerateResetToken(email, role string) (string, error) {
-	token := ResetToken{
-		Email:     email,
-		ExpiresAt: time.Now().Add(time.Minute * 30),
+
+	claim := &ClaimToRecover{
 		Role:      role,
+		Email:     email,
+		expiresAt: time.Now().Add(time.Minute * 30),
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 30)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
 	}
-
-	jsonData, err := json.Marshal(token)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	result, err := token.SignedString(secretKey)
 	if err != nil {
-		return "", err
+		return "error", err
 	}
+	result = strings.ReplaceAll(result, "+", "-")
+	result = strings.ReplaceAll(result, "/", "_")
+	return result, nil
+	// jsonData, err := json.Marshal(token)
+	// if err != nil {
+	// 	return "", err
+	// }
 
-	// Подписываем токен
-	mac := hmac.New(sha256.New, secretKey)
-	mac.Write(jsonData)
-	signature := mac.Sum(nil)
+	// // Подписываем токен
+	// mac := hmac.New(sha256.New, secretKey)
+	// mac.Write(jsonData)
+	// signature := mac.Sum(nil)
 
-	// Формат: данные.подпись
-	return fmt.Sprintf(
-		"%s.%s",
-		base64.URLEncoding.EncodeToString(jsonData),
-		base64.URLEncoding.EncodeToString(signature),
-	), nil
+	// // Формат: данные.подпись
+	// return fmt.Sprintf(
+	// 	"%s.%s",
+	// 	base64.URLEncoding.EncodeToString(jsonData),
+	// 	base64.URLEncoding.EncodeToString(signature),
+	// ), nil
 }
 
-func ValidateResetToken(tokenStr string) (*ResetToken, error) {
-	parts := strings.Split(tokenStr, ".")
-	if len(parts) != 2 {
-		return nil, fmt.Errorf("invalid token format")
-	}
+func ValidateResetToken(tokenString string) (*ClaimToRecover, error) {
 
-	// Декодируем данные
-	jsonData, err := base64.URLEncoding.DecodeString(parts[0])
+	claim := &ClaimToRecover{}
+	token, err := jwt.ParseWithClaims(tokenString, claim, func(t *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
 	if err != nil {
-		return nil, fmt.Errorf("invalid token encoding")
+		return nil, err
 	}
 
-	// Проверяем подпись
-	mac := hmac.New(sha256.New, secretKey)
-	mac.Write(jsonData)
-	expectedSignature := mac.Sum(nil)
+	if !token.Valid {
 
-	actualSignature, err := base64.URLEncoding.DecodeString(parts[1])
-	if err != nil {
-		return nil, fmt.Errorf("invalid signature encoding")
+		return nil, err
 	}
+	return claim, nil
+	// parts := strings.Split(tokenStr, ".")
+	// if len(parts) != 2 {
+	// 	return nil, fmt.Errorf("invalid token format")
+	// }
 
-	if !hmac.Equal(actualSignature, expectedSignature) {
-		return nil, fmt.Errorf("invalid token signature")
-	}
+	// // Декодируем данные
+	// jsonData, err := base64.URLEncoding.DecodeString(parts[0])
+	// if err != nil {
+	// 	return nil, fmt.Errorf("invalid token encoding")
+	// }
 
-	// Парсим данные
-	var token ResetToken
-	if err := json.Unmarshal(jsonData, &token); err != nil {
-		return nil, fmt.Errorf("invalid token data")
-	}
+	// // Проверяем подпись
+	// mac := hmac.New(sha256.New, secretKey)
+	// mac.Write(jsonData)
+	// expectedSignature := mac.Sum(nil)
 
-	// Проверяем срок действия
-	if time.Now().After(token.ExpiresAt) {
-		return nil, fmt.Errorf("token expired")
-	}
+	// actualSignature, err := base64.URLEncoding.DecodeString(parts[1])
+	// if err != nil {
+	// 	return nil, fmt.Errorf("invalid signature encoding")
+	// }
 
-	return &token, nil
+	// if !hmac.Equal(actualSignature, expectedSignature) {
+	// 	return nil, fmt.Errorf("invalid token signature")
+	// }
+
+	// // Парсим данные
+	// var token ResetToken
+	// if err := json.Unmarshal(jsonData, &token); err != nil {
+	// 	return nil, fmt.Errorf("invalid token data")
+	// }
+
+	// // Проверяем срок действия
+	// if time.Now().After(token.ExpiresAt) {
+	// 	return nil, fmt.Errorf("token expired")
+	// }
+
+	// return &token, nil
 }
